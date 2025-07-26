d8efd5dfaf4ca63fb58487fdee5d637b5287b989
Nikilesha <nikilesha.nivas@gmail.com>
2025-07-27 01:36:26 +0530

    Time: 4 ms (100.00%) | Memory: 44 MB (60.72%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -2,8 +2,6 @@ class Solution {
     public boolean isPalindrome(int x) {
         if(x<0)
            return false;
-        if(x==0)
-            return true;
         int temp = x;
         int rev = 0;
         while(x != 0){
