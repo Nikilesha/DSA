ae537d0c0e9c040a948aa9c785eef7619ea5e905
Nikilesha <nikilesha.nivas@gmail.com>
2026-07-03 13:20:16 +0530

    Time: 1 ms (99.86%) | Memory: 46.7 MB (65.20%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -1,9 +1,6 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-        if(n == 0){
-            return new ArrayList<>();
-
-        }
+        
         List<String> list = new ArrayList<>();
         for(int i =1;i<=n;i++){
             if(i%3==0 && i%5 == 0){
