76e45d8262c4d491be50deebd4e041b2555923f7
Nikilesha <nikilesha.nivas@gmail.com>
2026-07-03 12:41:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (8.29%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs == null || strs.length == 0){
+            return "";
+        }
+        String check = strs[0];
+
+        for(int i =1;i<strs.length;i++){
+            while(strs[i].indexOf(check) != 0){
+                check = check.substring(0,check.length()-1);
+
+                if(check.length()==0){
+                    return "";
+                }
+            }
+            
+        }
+
+        return check;
+        
+    }
+}
